8139e8c7de78ea0ab64ab2b77e502b9bfa7ab0bc
Emily Oppenheimer <opemily@gmail.com>
2015-11-04 15:34:54 -0500

    Started JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-$(function(){
+$(function () {
+
+   var  logList= [],
+        $log = $('#log'),
+        $prompt = $('#prompt');
 
 
 
-  
-})
+$prompt.keypress(function( event ) {
+  if ( event.which == 13 ) {
+     promptCommand = $prompt.val();
+     //console.log(promptCommand);
+     $log.append('<li class="command">' + promptCommand +'</li>');
+     $prompt.val('');
+
+  } 
+});
+
+
+
+
+
+});
