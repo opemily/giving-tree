32ca539fc75af8fe969548d5261196da30d2b680
Emily Oppenheimer <opemily@gmail.com>
2015-11-04 15:41:40 -0500

    Finish Act 1

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $(function () {
    var  logList= [],
         $log = $('#log'),
         $prompt = $('#prompt');
+        apples = 10000;
 
 
 
@@ -12,6 +13,9 @@ $prompt.keypress(function( event ) {
      //console.log(promptCommand);
      $log.append('<li class="command">' + promptCommand +'</li>');
      $prompt.val('');
+     if (promptCommand === 'apples'){
+      $log.append('<li class="action"> The Giving Tree has ' + apples +' apples on her branches</li>')
+     }
 
   } 
 });
