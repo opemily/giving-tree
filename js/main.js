93f0afd48c212b8964a9878203a031539aabc2d0
Emily Oppenheimer <opemily@gmail.com>
2015-11-05 08:33:01 -0500

    Act 2 and some of Act 3

--- js/main.js
+++ js/main.js
@@ -1,27 +1,59 @@
+
+var $log = $('#log'),
+    $prompt = $('#prompt'),
+    givingTree = {
+        appleCount: 1000
+    };
+
 $(function () {
 
-   var  logList= [],
-        $log = $('#log'),
-        $prompt = $('#prompt');
-        apples = 10000;
+    var appleCounter = function () {
+
+        $log.append('<li class="action"> The Giving Tree has ' + givingTree.appleCount + ' apples on her branches</li>');
+    };
+
+    var appleGiver = function () {
+
+        $log.append('<li class="action"> The Giving Tree ' + promptWords[0] + 's ' + promptWords[1] + ' ' + promptWords[2] + ' ' + promptWords[3] + '</li>');
+
+        givingTree.appleCount -= parseInt(promptWords[2]);
+    };
+
+    var recipient = function () {
+
+        givingTree.recipient = [promptWords[1], promptWords[2]];
+    };
 
 
+    $prompt.keypress(function (event) {
+        if (event.which === 13) {
 
-$prompt.keypress(function( event ) {
-  if ( event.which == 13 ) {
-     promptCommand = $prompt.val();
-     //console.log(promptCommand);
-     $log.append('<li class="command">' + promptCommand +'</li>');
-     $prompt.val('');
-     if (promptCommand === 'apples'){
-      $log.append('<li class="action"> The Giving Tree has ' + apples +' apples on her branches</li>')
-     }
+            promptCommand = $prompt.val();
+            $log.append('<li class="command">' + promptCommand + '</li>');
+            $prompt.val('');
 
-  } 
-});
+          // Act 1
 
+            if (promptCommand === 'apples') {
 
+                appleCounter();
+            }
 
+          // Act 2
 
+            promptWords = promptCommand.split(' ');
 
+            if (promptWords[0] === 'give') {
+
+                appleGiver();
+                recipient();
+            }
+
+          // Act 3
+
+            if (promptWords[0] === 'ledger') {
+
+            }
+        }
+    });
 });
